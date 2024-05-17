9fa8ee5bc07873e6d8317ff4f1902bec26a4ae97
Arvind <pingarvindforquestions@gmail.com>
2024-05-17 06:00:31 +0000

    Added: for requirements.txt

--- src/commands/export.sh
+++ src/commands/export.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+
+# For requirements.txt
+echo "Exporting requirements.txt"
 poetry export --without-hashes --format=requirements.txt > requirements.txt
+
+# For requirements-dev.txt
+echo "Exporting requirements-dev.txt"
+poetry export --without-hashes --with dev --format=requirements.txt > requirements-dev.txt
