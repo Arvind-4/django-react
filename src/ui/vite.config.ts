440eb9b0d46d6eee515403ad4e1a3b5c776db779
Arvind <pingarvindforquestions@gmail.com>
2024-05-20 09:39:17 +0000

    Updated: modified the ui code

--- src/ui/vite.config.ts
+++ src/ui/vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const baseUrl = process.env.NODE_ENV === "production" ? "/static/" : "";
+let baseUrl = process.env.NODE_ENV === "production" ? "/static/" : "/";
+
+
+const isDjango = process.env.DJANGO === "true";
+
+if (isDjango) {
+  baseUrl = "/static/";
+} else {
+  baseUrl = "/";
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
