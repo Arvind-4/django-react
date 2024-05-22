7e6392c5825ecd5d32cb9c6941865ce6e6f09b17
Arvind <pingarvindforquestions@gmail.com>
2024-05-22 10:18:51 +0000

    Updated: db for vercel

--- src/backend/db.py
+++ src/backend/db.py
@@ -1,12 +1,17 @@
+from backend.env import config
 from backend.settings.base import BASE_DIR
+
+DJANGO_VERCEL = config("DJANGO_VERCEL", default=True, cast=bool)
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+if DJANGO_VERCEL:
+    DATABASES = {}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
     }
-}
